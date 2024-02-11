4086c0ee51283c0bafa47f89b8ef32077d0a9f9c
Tom Edwards <t.edwards@enscape3d.com>
2024-02-11 16:05:53 +0100

    Added ItemsSourceView.Filter Added filter processing for Add and Remove collection changes Added search filter to ControlCatalog's ListBoxPage

--- samples/ControlCatalog/Pages/ListBoxPage.xaml.cs
+++ samples/ControlCatalog/Pages/ListBoxPage.xaml.cs
@@ -6,15 +6,33 @@ namespace ControlCatalog.Pages
 {
     public class ListBoxPage : UserControl
     {
+        private readonly ListBox _listBox;
+        private readonly TextBox _searchBox;
+
         public ListBoxPage()
         {
             InitializeComponent();
             DataContext = new ListBoxPageViewModel();
+
+            _listBox = this.Get<ListBox>("ListBox");
+
+            _searchBox = this.Get<TextBox>("SearchBox");
+            _searchBox.TextChanged += OnSearchTextChanged;
         }
 
         private void InitializeComponent()
         {
             AvaloniaXamlLoader.Load(this);
+        }
+
+        private void OnSearchTextChanged(object? sender, TextChangedEventArgs e)
+        {
+            _listBox.Items.Filter = string.IsNullOrEmpty(_searchBox.Text) ? null : FilterItem;
+        }
+
+        private void FilterItem(object? sender, ItemSourceViewFilterEventArgs e)
+        {
+            e.Accept = ((ItemModel)e.Item!).ToString().Contains(_searchBox.Text!);
         }
     }
 }
