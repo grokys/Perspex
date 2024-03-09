dc8275cbb22c08d6766e76eba22504629f782604
Tom Edwards <t.edwards@enscape3d.com>
2024-03-09 12:19:05 +0100

    Reuse the same EventArgs object for all items in a collection Store the layer's original "State" value on EventArgs objects

--- samples/ControlCatalog/Pages/ListBoxPage.xaml.cs
+++ samples/ControlCatalog/Pages/ListBoxPage.xaml.cs
@@ -6,13 +6,9 @@ namespace ControlCatalog.Pages
 {
     public class ListBoxPage : UserControl
     {
-        private readonly TextBox _searchBox;
-
         public ListBoxPage()
         {
             InitializeComponent();
-            _searchBox = this.Get<TextBox>("SearchBox");
-
             DataContext = new ListBoxPageViewModel();
         }
 
@@ -23,14 +19,14 @@ namespace ControlCatalog.Pages
 
         private void FilterItem(object? sender, FunctionItemFilter.FilterEventArgs e)
         {
-            if (string.IsNullOrEmpty(_searchBox.Text))
+            if (e.FilterState is not string { Length: > 0 } searchText)
             {
                 e.Accept = true;
             }
             else
             {
                 var item = (ItemModel)e.Item!;
-                e.Accept = item.IsFavorite || item.ID.ToString().Contains(_searchBox.Text);
+                e.Accept = item.IsFavorite || item.ID.ToString().Contains(searchText);
             }
         }
 
